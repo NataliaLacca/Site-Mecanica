4fe271acf0fc30af9a88b6879066af1617804172
Natália Lacca <nathy.lacca@gmail.com>
2023-01-09 21:34:47 -0300

    Initial commit

---
+++ conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "oficina";
+$port = 3306;
+
+try{
+	$conn = new PDO ("mysql:host=$host;port=$port;dbname=" . $dbname,$user,$pass);
+	
+		echo "Conexão com banco de dados realizada com sucesso!";
+}catch(PDOException $erro){
+	echo "Erro: Conexão com banco de dados não realizada" . $erro;
+}
+
+?>
